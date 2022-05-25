3131f792ee8cabe847d3036ebe231391802d300a
Jakko-Jan <jakko-jan.vanek@wur.nl>
2022-05-25 09:11:46 +0200

    Added main file; added load_data_from_zip_folders to extract data from zip folders to a destination folder; added load_images to load images from destination folder as rasterio dataset readers. Script needs to have data in a folder containing multiple zip folders in order to work

---
+++ load_data_from_zip_folders.py
@@ -0,0 +1,38 @@
+#############
+## LOAD IMAGES FROM ZIP FILE
+############
+
+import rasterio as rio
+from rasterio.plot import show
+import os
+import datetime 
+import numpy as np
+import matplotlib.pyplot as plt
+from osgeo import gdal
+from zipfile import ZipFile
+
+def load_data(dir_name='sentinelhub_downloads', dest_name = 'radar_time_series'):
+#dir_name =  folder containing multiple zip folders
+# dest_name =   #destination folder
+    if not os.path.exists(dest_name): os.makedirs(dest_name)
+    extension = ".zip"
+    image_names = []
+    
+    #Unzip images to a new folder
+    for item in os.listdir(dir_name): # loop through items in dir
+        if item.endswith(extension): # check for ".zip" extension
+            file_name =  dir_name + "/" + item  # get full path of files
+            zip_ref = ZipFile(file_name) # create zipfile object
+            zipinfos=zip_ref.infolist()
+            for zipinfo in zipinfos:
+                #change filename to something shorter
+                date = zipinfo.filename[0:10]
+                info = zipinfo.filename[-79:]
+                zipinfo.filename = date + '_' + info
+                image_names.append(zipinfo.filename)
+                #extract image to destination folder
+                zip_ref.extract( zipinfo, dest_name)
+            zip_ref.close() # close file
+            #os.remove(file_name) # delete zipped file            
+    return(image_names)       
+
---
+++ load_images.py
@@ -0,0 +1,15 @@
+import rasterio as rio
+from rasterio.plot import show
+import os
+
+##load images from folder using rasterio
+##the function returns a list containing all rasterio dataset readers
+
+def load_images(image_names, dest_name='radar_time_series'):
+    if not os.path.exists(dest_name): os.makedirs(dest_name)
+
+
+    #Load images from folder using rasterio package:
+    rasters = [rio.open(dest_name+'/'+filename) for filename in image_names]
+    #show(rasters[0], title=str(rasters[0]), cmap='gist_ncar')
+    return(rasters)

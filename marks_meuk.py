bc2338768ae8c7534a5441c215e1da44be42b919
Mark Boeve <mark.boeve@wur.nl>
2022-05-25 14:07:00 +0200

    oke

--- marks_meuk.py
+++ marks_meuk.py
@@ -5,12 +5,15 @@ Created on Wed May 25 11:52:31 2022
 @author: markb
 """
 
-import rasterio as rs   
+import rasterio as rs
 import numpy as np
 from osgeo import gdal, ogr
+import matplotlib.pyplot as plt
+import os
+import seaborn as sns
+
 
 # Open dataset
-dataset = gdal.Open(r'C:\Users\markb\Downloads\W080N20_PROBAV_LC100_global_v3.0.1_2019-nrt_Discrete-Classification-map_EPSG-4326')
+dataset = rs.open(r'C:\Users\markb\Downloads\W080N20_PROBAV_LC100_global_v3.0.1_2019-nrt_Discrete-Classification-map_EPSG-4326.tif')
 
-
-print(dataset.RasterCount)
+band1 = dataset.read(1)

952f6c60c4d38ccc16115f8f1a7e1995d2c0dbad
Verouden, Niels <niels.verouden@wur.nl>
2022-06-10 09:10:11 +0000

    Update DEM.py

--- DEM.py
+++ DEM.py
@@ -18,17 +18,28 @@ caphaitien_bbox = BBox(bbox = caphaitien_coords, crs = CRS.WGS84)
 caphaitien_size = bbox_to_dimensions(caphaitien_bbox, resolution = resolution)
 """
 
-os.chdir('C:/Users/julia/Documents/WUR/ACT/') 
+# os.chdir('C:/Users/julia/Documents/WUR/ACT/') 
 
 
-#load DEM from files
-fp = r'C:/Users/julia/Documents/WUR/ACT/DEM/Haiti2.tif'
-img = rasterio.open(fp)
-show(img)
+# load DEM from files
+# fp = r'C:/Users/julia/Documents/WUR/ACT/DEM/Haiti2.tif'
+# img = rasterio.open(fp)
+# show(img)
 
+# bbox = (-72.273903,19.671135,-72.159233,19.798714)
+
+# gdal.Translate('DEM/Haiti_cropped.tif', fp, projWin=bbox)
+
+
+# Crop DEM
 bbox = (-72.273903,19.671135,-72.159233,19.798714)
+input_file = "./Data/Haiti2.tif"
+output_file = "./Output/DEM_crop.tif"
 
-gdal.Translate('DEM/Haiti_cropped.tif', fp, projWin=bbox)
+ds = gdal.Open(input_file)
+DEM_crop = gdal.Warp(output_file, ds, options=gdal.WarpOptions(
+    options=['outputBounds'], outputBounds=bbox))
 
-
+img = rasterio.open(output_file)
+show(img)
 

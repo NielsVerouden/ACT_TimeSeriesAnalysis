34dd36ab0c92dbb4f860d5431497ed962c11404f
Mark Boeve <mark.boeve@wur.nl>
2022-06-10 12:34:09 +0200

    nog meer struggles in python bijna weekend

--- water.py
+++ water.py
@@ -5,14 +5,14 @@ import pandas as pd
 import numpy as np
 import fiona
 import rasterio
-import rasterio.mask
+from rasterio.mask import mask
 from rasterio.mask import mask
 import gdal
+from rasterio.plot import show
+from shapely.geometry import mapping
 
 # Open dataset
 test = rs.open(r'data/really_small_area/really_small.tif')
-
-whole = rs.open(r'data/occurrence_80W_20Nv1_3_2020.tif')
 
 # PLot data
 plt.imshow(test.read(1), cmap='pink')
@@ -20,13 +20,11 @@ plt.imshow(test.read(1), cmap='pink')
 # Create an array
 test_arr = test.read(1)
 
-# PLot the water data
+# PLot the water data array
 plt.imshow(test_arr)
-
 
 # Create metadata about the file to save it
 metadata = test.profile
-
 
 metadata.update(
         dtype=rs.uint8,
@@ -46,13 +44,73 @@ def create_mask (water_data):
                 water_data[i][j] = 1
     return(water_data)
 
-create_mask(test_arr)
+masked = create_mask(test_arr)
             
 # Plot the result
 plt.imshow(test_arr)
 
-# Set crs
-crs=test.crs
+
+
+
+
+
+
+
+
+    
+
+# Open landuse map
+landuse = rs.open("data/2022-02-01_stack.tiff")
+
+# Create landuse array
+landuse_arr = landuse.read()
+
+# Maintain only the first layer
+landuse_arr = landuse_arr[1]
+    
+# Plot the landuse map
+show(landuse_arr)
+
+
+
+# Mask the water bodies
+with rs.open("./data/2022-02-01_stack.tiff") as landuse:
+    out_image, out_transform = mask(landuse, test_arr, crop=True)
+
+
+
+
+
+
+
+
+
+
+with rasterio.open('data/really_small_area/really_small.tif') as dataset:
+    mask = test.dataset_mask()
+    
+    for geom, val in rasterio.features.shapes(
+        mask, transform=dataset.transform):
+        # Transform shapes from the dataset's own coordinate
+        # reference system to CRS84 (EPSG:4326).
+        geom = rasterio.warp.transform_geom(
+            dataset.crs, 'EPSG:4326', geom, precision=6)
+
+      # Print GeoJSON shapes to stdout.
+    shapes = geom
+    
+df = pd.DataFrame(shapes)
+
+geoms = df.coordinates
+    
+small_crop = [-72.206603569, -72.203443465, 19.7478899750001, 19.7503596370001]
+
+
+
+caphaitien_coords = [-72.273903,19.671135,-72.159233,19.798714]
+
+small_crop = [-72.206603569, -72.203443465, 19.7478899750001, 19.7503596370001]
+
 
 # Create and save as raster file again
 def save_raster(file_path, array,crs):
@@ -67,50 +125,3 @@ def save_raster(file_path, array,crs):
                     nodata=None, # change if data has nodata value
                     transform=test.transform) as dst:
         dst.write(test_arr, 1)
-    
-
-# Open landuse map
-landuse = rs.open("data/2022-02-01_stack.tiff", driver="GTiff")
-landuse_arr = landuse.read()
-
-# Maintain only the first layer
-landuse_arr = landuse_arr[0]
-    
-# Plot the landuse map
-plt.imshow(landuse_arr)
-
-with rasterio.open('data/really_small_area/really_small.tif') as dataset:
-    mask = test.dataset_mask()
-    
-    for geom, val in rasterio.features.shapes(
-        mask, transform=dataset.transform):
-        # Transform shapes from the dataset's own coordinate
-        # reference system to CRS84 (EPSG:4326).
-        geom = rasterio.warp.transform_geom(
-            dataset.crs, 'EPSG:4326', geom, precision=6)
-
-      # Print GeoJSON shapes to stdout.
-    shapes = geom
-
-# Crop DEM
-bbox = (-72.273903,19.671135,-72.159233,19.798714)
-input_file = "data/2022-02-01_stack.tiff"
-output_file = "./data/landuse_crop.tif"
-
-ds = gdal.Open(input_file)
-landuse_crop = gdal.Warp(output_file, ds, options=gdal.WarpOptions(
-    options=['outputBounds'], outputBounds=small_crop))
-
-img = rasterio.open(output_file)
-plt.show(img)
-
-
-
-
-out_image, out_transform = mask(landuse_arr, shapes, crop=True)
-
-
-
-caphaitien_coords = [-72.273903,19.671135,-72.159233,19.798714]
-
-small_crop = [-72.206603569, -72.203443465, 19.7478899750001, 19.7503596370001]

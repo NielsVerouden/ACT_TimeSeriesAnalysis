dc3f69955bf366205a7c8b5ef838b08b333bf34b
Jakko-Jan <jakko-jan.vanek@wur.nl>
2022-06-14 09:40:25 +0200

    Implemented random forest function. Created code to separately produce frequency maps for urban floods, flooded land and combination.

--- train_model.py
+++ train_model.py
@@ -1,6 +1,7 @@
 # Train a machine learning model
 
 from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
 
 
 #Train a GaussianNaiveBayes Classifier (just for trying)
@@ -11,3 +12,13 @@ def GaussianNaiveBayes(X,y):
     return gnb
 #credit: http://patrickgray.me/open-geo-tutorial/chapter_5_classification.html
 #https://towardsdatascience.com/land-cover-classification-in-satellite-imagery-using-python-ae39dbf2929
+
+def RandomForest(X,y, n_estimators=100, criterion="gini",max_depth=None):
+    rf = RandomForestClassifier(n_estimators=n_estimators,
+                                criterion=criterion,        #"gini", "entropy", "log_loss"
+                                max_depth=max_depth)        #None or an integer value
+    rf.fit(X,y)
+    return rf                            
+#see the following link for all random forest options:
+#https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html
+    

7d697cf0e52e0bac6f05afbb1e6971131e1067d0
Verouden, Niels <niels.verouden@wur.nl>
2022-06-26 23:42:24 +0000

    Update s11ClimateDataAPI.py

--- urban_areas/py/s11ClimateDataAPI.py
+++ urban_areas/py/s11ClimateDataAPI.py
@@ -15,36 +15,34 @@ import os
 import requests
 import csv
 import datetime
+import geopandas as gpd
+import rasterio as rio
+from rasterio.plot import show
+import matplotlib.pyplot as plt
+from shapely.geometry import Point
 
-def climateDataAPI(longitude, latitude, start, end):
-    # Defines grid of lon/lat coordinates for which ppt data is downloaded. 
-    # Either 3 or 4 is recommended
-    steps = 3
-
+def visualiseCoordinates(coordinates, grid, start, end, visualise, SAR_path):
     # Create directory
     if not os.path.exists(os.path.join('data', 'precipitation_data')):
         os.makedirs(os.path.join('data', 'precipitation_data'))
-    
+
     # Define start and end date
     start = datetime.datetime.strptime(start, '%Y-%m-%d')
     start = int(start.strftime("%Y%m%d"))
     end = datetime.datetime.strptime(end, '%Y-%m-%d')
     end = int(end.strftime("%Y%m%d"))
 
-    # Define boundingbox around given lon and lat
-    minx, miny = ((i - 0.1) for i in (longitude, latitude))
-    maxx, maxy = ((i + 0.1) for i in (longitude, latitude))
-    
     # Create a spacing based on the steps defined above
-    x_spacing = ([maxx, maxy][0] - [minx, miny][0]) / (steps + 1)
-    y_spacing = ([maxx, maxy][1] - [minx, miny][1]) / (steps + 1)
+    minx, miny, maxx, maxy = coordinates
+    x_spacing = ([maxx, maxy][0] - [minx, miny][0]) / (grid-1)
+    y_spacing = ([maxx, maxy][1] - [minx, miny][1]) / (grid-1)
 
     # Assign the latitudes and longitudes to different lists
-    latitudes = [[minx, miny][1] +  i * y_spacing for i in range(1, steps+1)]
-    longitudes = [[minx, miny][0] + i * x_spacing for i in range(1, steps+1)]
+    latitudes = [[minx, miny][1] +  i * y_spacing for i in range(0, grid)]
+    longitudes = [[minx, miny][0] + i * x_spacing for i in range(0, grid)]
 
     # Create list where all lat/lon combinations are stored
-    coordinates = []
+    coordinates_list = []
 
     # Make empty list where all requests should be stored
     request_list = []
@@ -56,8 +54,47 @@ def climateDataAPI(longitude, latitude, start, end):
             file_name = f'daily_{start}_{end}_loc{count}.CSV'
             # Append request names to list
             request_list.append((request, file_name))
-            coordinates.append([lon, lat])
+            coordinates_list.append([lon, lat])
             count+=1
+   
+    ###### Visualise coordinates on SAR data
+    if visualise == 'y':
+        # Append geometry.Point objects to points list, for conversion to Geopandas DataFrame
+        points = []
+        for coordinates in coordinates_list:
+            pnt = Point(coordinates[0], coordinates[1])
+            points.append(pnt)
+        
+        # Create Geopandas DataFrame for the lat/lon combinations
+        gdf = gpd.GeoDataFrame(geometry=points, crs=4326)
+        
+        # Define path to open the first SAR image
+        path = os.path.join('data', 'SAR_data', SAR_path)
+        files = os.listdir(path)
+        file_path = os.path.join(path, files[0])
+        
+        # Open first SAR image in files
+        src = rio.open(file_path)
+        
+        ## Plot results
+        fig, ax = plt.subplots(figsize=(10, 10))
+        # transform rasterio plot to real world coords
+        extent=[src.bounds[0], src.bounds[2], src.bounds[1], src.bounds[3]]
+        ax = rio.plot.show(src, extent=extent, ax=ax)
+        gdf.plot(ax=ax, color='w', 
+                 marker = 'o',
+                 markersize=100)
+        plt.title(f'VV {files[0][0:10]}', fontdict={'fontsize': 20})
+        plt.axis(False)
+    
+    return coordinates_list, request_list
+
+def climateDataAPI(request_list, start, end):
+    # Define start and end date
+    start = datetime.datetime.strptime(start, '%Y-%m-%d')
+    start = int(start.strftime("%Y%m%d"))
+    end = datetime.datetime.strptime(end, '%Y-%m-%d')
+    end = int(end.strftime("%Y%m%d"))
     
     file_names= []
     for request in request_list:
@@ -81,6 +118,6 @@ def climateDataAPI(longitude, latitude, start, end):
         # Write CSV file to directory
         with file:   
             write = csv.writer(file)
-            write.writerows(list(cr))
-            
-    return file_names, coordinates
+            write.writerows(list(cr))       
+    
+    return file_names
